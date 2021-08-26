a4f9541dabd3b62e3925e3d23af52782e033920c
Art-mj1 <mjmjpc@naver.com>
2021-08-26 09:28:25 +0900

    08/26 반응형 진행 중
    반응형 작업 중

--- js/jq.main.js
+++ js/jq.main.js
@@ -1,24 +1,39 @@
-$(function(){
+$(function () {
+  //header stick to top and change style when scrolling down
+  const headerStick = function () {
+    const hdTop = $("header").offset().top;
 
- //header stick to top and change style when scrolling down
- const headerStick = function(){
-  const hdTop = $("header").offset().top;
-
-   $(window).scroll(function(){
-   const scroll = $(window).scrollTop();
-   //console.log(scroll);
-   if(scroll >= hdTop){
-    $("header").css({
-     position:"fixed",
-     top:0,
-     width:"100%",
+    $(window).scroll(function () {
+      const scroll = $(window).scrollTop();
+      //console.log(scroll);
+      if (scroll >= hdTop) {
+        $("header").css({
+          position: "fixed",
+          top: 0,
+          width: "100%",
+        });
+        $("header").addClass("stick");
+      } else {
+        $("header").css({ position: "relative" });
+        $("header").removeClass("stick");
+      }
     });
-    $("header").addClass("stick");
-   } else{
-    $("header").css({position:"relative"});
-    $("header").removeClass("stick");
-   }
-   });
- }
- headerStick();
+  };
+  headerStick();
+});
+
+$(function () {
+  const cuttingTEXT = function () {
+    // console.log($(".featured_item").length);
+    for (let i = 0; i < $(".con").length; i++) {
+      const textLength = $(".con").eq(i).find("p").text();
+      // console.log(textLength);
+
+      $(".con")
+        .eq(i)
+        .find("p")
+        .text(textLength.substr(0, 40) + "...");
+    }
+  };
+  cuttingTEXT();
 });
